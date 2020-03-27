1f7da94f79fb1c75e6df369c76891482c63c3dcb
PolNikitka <mr.dark051@yandex.ru>
2020-03-27 17:15:24 +0300

    add recyclerFragment

---
+++ app/src/main/java/com/example/recyclertest/RecyclerFragment.java
@@ -0,0 +1,33 @@
+package com.example.recyclertest;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerFragment extends Fragment {
+
+    private RecyclerView mRecycler;
+
+    public static RecyclerFragment newInstance() {
+       return new RecyclerFragment();
+    }
+    
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fr_recycler, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mRecycler = view.findViewById(R.id.rv);
+    }
+}
